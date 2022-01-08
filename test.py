7b0080c3b55d30fe896ef549988c835e26658bd9
Nuclear03020704 <lahdjirayhan@gmail.com>
2022-01-09 01:39:23 +0700

    Modify test for new, refactored code

--- test.py
+++ test.py
@@ -1,23 +1,24 @@
 from worksheetgen.wg import Worksheet
-import requests, os
-# Instantiates the worksheet object and titles it
-
 
 # Instantiates the worksheet object and titles it
 ws = Worksheet(title='Example Worksheet')
-# Start section to the worksheet
-ws.start_section('Section-1')
-# Add problems
-ws.add_problem('What is 9 + 10?')
-ws.add_problem('What is the square root of 9?', whitespace=True)
-ws.end_section()
-ws.start_section(name='Section-2', description='This is section-2')
-ws.add_problem('f(x)=3x-5, f(2)=?', type='math')
-ws.add_whitespace()
-ws.add_problem('f(x)=3x-5, f(2)=?', type='math', size_px=35)
-#End section
-ws.end_section()
-#Add instructions
-ws.add_instruction("That's for the Worksheet.")
-# Export pdf to ws.pdf
-ws.write_pdf()
+
+# Add sections
+with ws.section('Section 1'):
+    ws.add_problem('What is 9 + 10?') # No answer provided
+
+    # Note that the raw string literal r'' is needed so that
+    # the backslashes are not escaped
+    ws.add_problem(r'What is the value of \( \sqrt{9} \) ?', 3) # Answer provided
+
+# Note that the numbering is restarted for each section
+with ws.section('Section 2', description="This is a description to section 2"):
+    # The following problem, '\tan' will be escaped into 'tab an' if not for the r''
+    ws.add_problem(r'What is the value of \( \tan(45^0) \)', whitespace_after=True) # Add whitespace
+    ws.add_problem(r'\( f(x)=3x-5,\; f(2)=? \)')
+
+# Instruction-class can also be used for general texts, I suppose?
+ws.add_instruction("That's for the worksheet!")
+
+# Write PDF with filename
+ws.write_pdf('ws.pdf')
