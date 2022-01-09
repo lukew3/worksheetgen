014b180e325327bf49387959f24a5b73be167717
Luke Weiler <lukew25073@gmail.com>
2022-01-09 15:44:17 -0500

    Update version to 0.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='worksheetgen',
-      version='0.0.5',
+      version='0.1.0',
       description='A worksheet generator',
       url='https://github.com/lukew3/worksheetgen',
       author='Luke Weiler',
